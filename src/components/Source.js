328d03151ca981b28da514e4fb9b7372f93ab2b9
Smith Scarborough <smithscarborough@gmail.com>
2021-04-15 13:34:15 -0500

    add additional data to each piece of source content

--- src/components/Source.js
+++ src/components/Source.js
@@ -1,18 +1,22 @@
 // testing this component out in place of SingleLineGridList.js (a material-ui component)
 import { Container } from '@material-ui/core'
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Source(props) {
     return (
         <div>
             <Container>
                 {props.content.map((pieceOfContent) => {
-                    const { title, creator } = pieceOfContent;
+                    const { title, creator, link, pubDate, contentSnippet } = pieceOfContent;
 
                     return (
                         <div>
-                            <h5>{title}</h5>
+                            <Link to={link}><h4>{title}</h4></Link>
                             <p>{creator}</p>
+                            <p>{pubDate}</p>
+                            <p>{contentSnippet}</p>
+                            --------------------
                         </div>
                     )
                 })}
