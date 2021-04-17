9f611b25ce5b9149767804d66246656203506647
Smith Scarborough <smithscarborough@gmail.com>
2021-04-16 23:06:12 -0500

    add space between navbar buttons by using a material ui Box component

--- src/components/SearchAppBar.js
+++ src/components/SearchAppBar.js
@@ -5,7 +5,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Button, Menu, MenuItem } from '@material-ui/core';
+import { Box, Button, Menu, MenuItem } from '@material-ui/core';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { withRouter } from 'react-router-dom';
 
@@ -93,11 +93,19 @@ const SearchAppBar = props => {
           </Menu>
           </>
           ) : (
-            <div className={classes.headerOptions}>
+            <div className={classes.headerOptions} >
+              <Box m={1}>
               <Button variant="contained" onClick={() => handleButtonClick('/')}>Home</Button>
+              </Box>
+              <Box m={1}>
               <Button variant="contained" onClick={() => handleButtonClick('/news')}>News</Button>
+              </Box>
+              <Box m={1}>
               <Button variant="contained" onClick={() => handleButtonClick('/business')}>Business</Button>
+              </Box>
+              <Box m={1}>
               <Button variant="contained" onClick={() => handleButtonClick('/tech')}>Tech</Button>
+              </Box>
             </div>
           )}
           </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,59 +6,11 @@ import Source from '../components/Source';
 
 
 export default function Home() {
-    // use state to pass down news content from a given source to the SingleLineGridList component to display the content
-    const [content, setContent] = useState({ items: [] });
-    const [outlet, setOutlet] = useState('');
-
-    // maybe the following should be useEffect instead...
-    const getRSSContent = (url) => {
-        setContent({ 
-            title: 'Loading...',
-            items: []
-        })
-        fetch(`https://cube-news-app.netlify.app/.netlify/functions/fetch-rss?url=${url}`)
-        .then((res) => res.json())
-        .then((data) => {
-            setContent(data);
-        })
-        .then((data) => console.log(data))
-    };
-
-       useEffect(() => {
-        getRSSContent(outlet);
-       }, [outlet]);
-
-    //    const changeOutlet = (e) => {
-    //     setOutlet(e.target.value)
-    //    }
 
 
     return (
         <Container>
-            <h3>Welcome to Cube News!</h3>
-            <h4>What content would you like to see?</h4>
-            <select>
-                <option key="news" value={<Link to="./News.js"/>}>News</option>
-                <option key="tech" value="./Tech.js">Tech</option>
-                <option>Business</option>
-                <option>Sports</option>
-            </select>
-            
-            
-            
-            
-            {/* <select onChange={changeOutlet}>
-                <option key="Mashable" value="https://mashable.com/rss/">Mashable</option>
-                <option key="Dev" value="https://dev.to/rss">DEV.to</option>
-                <option key="Engadget" value="https://www.engadget.com/rss.xml">Engadget</option>
-                <option key="CSS Tricks" value="https://css-tricks.com/feed/">CSS-Tricks</option>
-            </select> */}
-            {content.items.length ? (
-        
-                <Source content={content.items} title={content.title} />
-            ) : (
-                '     Loading... '
-            )}
+            <h3 textAlign="center">Welcome to Cube News!</h3>
         </Container>
     )
 }
