f256dd4daa2f0d23ace3ba553b4ee43bc47d6ebb
Smith Scarborough <smithscarborough@gmail.com>
2021-06-08 14:21:46 -0500

    remove copyright container from bottom of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,9 @@ export default function Home() {
             <h3>Welcome to Cube News!</h3>
             <h4>Because there's different sides to every story.</h4>
             <ThreeScene />
-            <Box position="absolute" bottom="0" width="100%" height="45px" align="center" paddingRight="95px">
+            {/* <Box position="absolute" bottom="0" width="100%" height="45px" align="center" paddingRight="95px" zIndex="-100">
                 <h5>&#169; Copyright 2021 Cube News</h5>
-            </Box>
+            </Box> */}
         </Container>
     )
 }
